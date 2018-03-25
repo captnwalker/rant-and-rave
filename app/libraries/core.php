e3ed7bbae48f62dbab4c395e35f11971a5c3afa3
Steve Walker <captnwalker@gmail.com>
2018-03-25 12:10:30 -0400

    mapped methods

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -6,7 +6,7 @@
     */
     class Core {
         protected $currentController = 'Pages';
-        protected $curretnMethod = 'index';
+        protected $currentMethod = 'index';
         protected $params = [];
 
         public function __construct() {
@@ -28,15 +28,31 @@
 
             // Instantiate controller class
             $this->currentController = new $this->currentController;
+
+            // Check for second part of URL
+            if(isset($url[1])){
+                // Check to see if method exists in controller
+                if(method_exists($this->currentController, $url[1])){
+                  $this->currentMethod = $url[1];
+                  // Unset 1 index
+                  unset($url[1]);
+                }
+            }
+
+        // Get params
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
         }
 
         // Get, Sanitize and Format URL returned in browser address bar
-        public function getUrl() {
+        public function getUrl(){
             if(isset($_GET['url'])){
-                $url = rtrim($_GET['url'], '/');
-                $url = filter_var($url, FILTER_SANITIZE_URL);
-                $url = explode('/', $url);
-                return $url;
+              $url = rtrim($_GET['url'], '/');
+              $url = filter_var($url, FILTER_SANITIZE_URL);
+              $url = explode('/', $url);
+              return $url;
             }
         }
     }
