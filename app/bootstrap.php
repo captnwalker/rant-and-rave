8d3866c907224de5bfafa6c581be03d03c658c33
Steve Walker <captnwalker@gmail.com>
2018-03-25 13:07:17 -0400

    bootstraped core library autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,11 @@
-<?php   
-// Load Libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+<?php
+    // Load Config
+    require_once 'config/config.php';
+
+    /*
+    * Autoloader for Core Libraries
+    * Loads ALL Core Libraries without having to require each one individually
+    */
+    spl_autoload_register(function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/rant-and-rave');
+    // Site name
+    define('SITENAME', 'Rant-and-Rave');
