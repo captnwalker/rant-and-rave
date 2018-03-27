dbedd58a4b2ca10633f49e9ed5661904c12b8466
Steve Walker <captnwalker@gmail.com>
2018-03-27 00:17:44 -0400

    posts form functional

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -20,13 +20,53 @@
             $this->view('posts/index', $data);
         }
 
-
         public function add(){
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-            $this->view('posts/add', $data);
+                // Sanitize post array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                  ];
+          
+                  // Validate data
+                  if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                  }
+                  if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                  }
+          
+                  // Make sure no errors
+                  if(empty($data['title_err']) && empty($data['body_err'])){
+
+                    // Validated
+                    if($this->postModel->addPost($data)){
+                      flash('post_message', 'Your post has been added');
+                      redirect('posts');
+
+                    } else {
+
+                      die('Something went wrong');
+                    }
+
+                  } else {
+                    // Load view with errors
+                    $this->view('posts/add', $data);
+                  }
+
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+
+                $this->view('posts/add', $data);
+            }
         }
     }
