e182201ba83c1423aa460bb2f1acb45b2ae583ca
Steve Walker <captnwalker@gmail.com>
2018-03-26 22:00:35 -0400

    add access control to posts controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,5 +1,12 @@
 <?php
     class Posts extends Controller {
+        // Check to see if user login to access posts page
+        public function __construct(){
+            if(!isLoggedin()){
+                redirect('users/login');
+            }
+        }
+
         public function index(){
             $data = [];
 
