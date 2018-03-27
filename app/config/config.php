99b0528e6e34d730285d7caf591d6e1c687e337b
Steve Walker <captnwalker@gmail.com>
2018-03-27 15:25:50 -0400

    config favicon for deploy

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
     // DB Params
-    define('DB_HOST', 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-    define('DB_USER', 'iug6r5w07yutpave');
-    define('DB_PASS', 'oq18moh4lzr69dg0');
-    define('DB_NAME', 'khwwvxe7d2tt7l06');
+    define('DB_HOST', 'ec2-54-243-63-13.compute-1.amazonaws.com');
+    define('DB_USER', 'rrrqzawhsanuyp');
+    define('DB_PASS', 'b4529cc7a9e64fae8122ee863cb854c7692b2a27c0b8ebf87f2f63d1b20f5e65');
+    define('DB_NAME', 'd8dlak95fcphm8');
 
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
