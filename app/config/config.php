4c37471e13e93891f2ca613689778bdd04c5cf1e
Steve Walker <captnwalker@gmail.com>
2018-03-27 09:01:52 -0400

    config for deploy

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
     // DB Params
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'ba6ba6');
+    define('DB_HOST', 'ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('DB_USER', 'q80hpl1bduib314q');
+    define('DB_PASS', 'jvi87kpmzzlczjwx');
     define('DB_NAME', 'rant');
 
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
-    define('URLROOT', 'http://localhost/rant-and-rave');
+    define('URLROOT', 'https://rant-and-rave.herokuapp.com/');
     // Site name
     define('SITENAME', 'Rant-and-Rave');
     // App Verison
