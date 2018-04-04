b88e4b3097714286e6cf2efe3574dd1857dd69c3
Steve Walker <captnwalker@gmail.com>
2018-04-04 16:11:16 -0400

    updated  pdo dependency

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     // DB Params
-    define("DB_HOST", "host");
-    define("DB_USER", "user");
-    define("DB_PASS", "pass");
-    define("DB_NAME", "db");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "ba6ba6");
+    define("DB_NAME", "rant");
 
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
-    define('URLROOT', 'https://rant-and-rave.herokuapp.com/');
+    define('URLROOT', 'http://localhost/rant-and-rave');
     // Site name
-    define('SITENAME', 'rant-and-rave');
+    define('SITENAME', 'Rant-and-Rave');
     // App Verison
     define('APPVERSION', '1.0.0');
